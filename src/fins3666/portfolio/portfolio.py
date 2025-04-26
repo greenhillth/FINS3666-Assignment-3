89dfb7749b90230688c251b46b871b12b6bf829d
Tom G <greenhill.tommo@gmail.com>
2025-04-26 21:56:12 +1000

    debugging portfolio class and implementing more currency conversion helper functions

--- src/fins3666/portfolio/portfolio.py
+++ src/fins3666/portfolio/portfolio.py
@@ -14,17 +14,27 @@ class Portfolio:
             unit_values (list or array, optional): Value of one unit of each asset.
             weights (list or array, optional): Portfolio weight for each asset. Should sum to 1.
         """
-        self.assets = assets
-        self.sizes = self._adjust_array(sizes, default=0)
-        self.positions = self._adjust_array(positions, default=0)
-        self.unit_values = self._adjust_array(unit_values, default=0)
-        self.asset_values = np.sum(self.sizes * self.unit_values)
-        self.total_value = np.sum(self.asset_value)
-        self.weights = self.asset_values / \
-            self.total_value if self.total_value > 0 else np.zeros(len(assets))
-        self.data = self._build_dataframe()
+        n = len(assets)
+        p = len(positions) if positions is not None else 0
+        pos = np.array([-1 if p.tolower.contains('short')
+                       else 1 for p in positions]) if p is n else np.ones(n)
 
-        # Portfolio Characteristics
+        # Asset Characteristics (Assigned)
+        self.assets = assets
+        sizes = self._adjust_array(sizes, default=0)
+        self.positions = self._adjust_array(pos, default=1)
+        self.sizes = np.array(
+            [-s if p < 0 else s for s, p in zip(sizes, self.positions)])
+        self.unit_values = self._adjust_array(unit_values, default=0)
+
+        # Asset Characteristics (Determined)
+        self.asset_values = np.sum(self.sizes * self.unit_values)
+        total_value = np.sum(self.asset_values)
+        self.weights = self.asset_values / \
+            total_value if total_value > 0 else np.zeros(len(assets))
+
+        # Portfolio Characteristics (Scalar)
+        self.total_value = total_value
         self.timestamp = timestamp
         self.sharpe_ratio = None
         self.sortino_ratio = None
@@ -33,22 +43,65 @@ class Portfolio:
         self.beta = None
         self.alpha = None
 
-    """Private methods for Portfolio class"""
+        self.data = self._build_dataframe()
 
-    def _update(self, sizes=None, positions=None, unit_values=None):
+    def add_asset(self, asset, size, unit_value, position='long'):
         """
-        Update the portfolio with new values.
+        Add a new asset to the portfolio.
 
         Args:
-            sizes (list or array, optional): New sizes for each asset.
-            positions (list or array, optional): New positions for each asset.
-            unit_values (list or array, optional): New unit values for each asset.
-            weights (list or array, optional): New weights for each asset.
+            asset (str): Asset identifier.
+            unit_value (float): Sale value of one unit of the asset, in USD.
+            size (float): Units of asset aquired.
+            position (string, optional): Defaults to 'long'. Specify 'short' in the case of a short-sale.
         """
-        self.sizes = self._adjust_array(sizes, default=self.sizes)
-        self.positions = self._adjust_array(positions, default=self.positions)
-        self.unit_values = self._adjust_array(
-            unit_values, default=self.unit_values)
+
+        # get or generate index of asset
+        idx = self._get_asset_idx(asset)
+
+        if position.tolower.contains('short'):
+            size = -size
+
+        self.sizes[idx] += size
+        self.unit_values[idx] = unit_value
+
+        self._update()
+
+    """Private methods for Portfolio class"""
+
+    def _get_asset_idx(self, asset):
+        """
+        Get the index of an asset in the portfolio, or add one if it doesn't exist.
+
+        Args:
+            asset (str): Asset identifier.
+
+        Returns:
+            int: Index of the asset in the portfolio.
+        """
+        if asset not in self.assets:
+            self.assets.append(asset)
+            self.sizes = np.append(self.sizes, 0)
+            self.positions = np.append(self.positions, 1)
+            self.unit_values = np.append(self.unit_values, 0)
+            self.weights = np.append(self.weights, np.nan)
+        return self.assets.index(asset)
+
+    def _update(self):
+        """
+        Update derived portfolio characteristics.
+
+        Args:
+            None
+        """
+        self.positions = np.where(self.sizes >= 0, 1, -1)
+        self.unit_values *= self.positions
+        self.asset_values = np.sum(self.sizes * self.unit_values)
+        self.total_value = np.sum(self.asset_values)
+        self.weights = self.asset_values / \
+            self.total_value if np.fabs(
+                self.total_value) > 1e-6 else np.zeros(len(self.assets))
+        self.data = self._build_dataframe()
 
     def _adjust_array(self, arr, default=0):
         """Ensure an array is the same length as assets, filling or truncating as needed."""
@@ -66,7 +119,7 @@ class Portfolio:
         df["Size"] = self.sizes
         df["UnitValue"] = self.unit_values
         df["AssetValue"] = self.sizes * self.unit_values
-        df["Position"] = self.positions
+        df["Position"] = 'Short' if self.sizes < 0 else 'Long'
         df["Weight"] = self.weights
         return df
 
--- src/fins3666/utils/functions.py
+++ src/fins3666/utils/functions.py
@@ -17,9 +17,63 @@ def load_dataframe(path):
     """
     df = pd.read_csv(path)
     df['Period'] = pd.to_datetime(df['Period'], format='%m/%Y')
+    df.sort_values(by='Period', ascending=True, inplace=True)
     df['Period'] = df['Period'].dt.to_period('M')
     df.set_index('Period', inplace=True)
     return df
+
+
+def getExchangeRate(df, termCurrency, baseCurrency='USD'):
+    """
+    Gets the exchange rate between two currencies from a DataFrame.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing exchange rates.
+        termCurrency (str): The term currency.
+        baseCurrency (str, optional): The base currency. Defaults to 'USD'.
+
+    Returns:
+        float: The exchange rate.
+    """
+    if baseCurrency == termCurrency:
+        return 1.0
+    invert = False
+    # if dataframe contains multiple entries, get the most recent quote
+    if isinstance(df, pd.DataFrame):
+        df = df.iloc[-1]
+    # check if the column exists in the series
+    col = f'{baseCurrency}/{termCurrency}'
+    if col not in df.keys():
+        col = f'{termCurrency}/{baseCurrency}'
+        invert = True
+    if col not in df.keys():
+        raise ValueError(
+            f"Exchange rate for {baseCurrency} to {termCurrency} not found in DataFrame.")
+    else:
+        return df[col] if not invert else df[col]**(-1)
+
+
+def currencyUSDvals(df, timestamp, currencies):
+    """
+    Converts currency values to USD using exchange rates from a DataFrame.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing exchange rates.
+        timestamp (str): The timestamp for the exchange rate.
+        currencies (list): List of currencies to convert.
+
+    Returns:
+        dict: Dictionary with currency values in USD.
+    """
+    period = pd.Period(timestamp, freq='M')
+    # round period down to the closest index present in the DataFrame
+    period = df.index[df.index <= period].max()
+    if period is pd.NaT:
+        raise ValueError(
+            f"Unable to locate sufficiently accurate FX data for time {timestamp.to_string()}.")
+
+    df = df.loc[period]
+    return [getExchangeRate(df, baseCurrency=c, termCurrency='USD') for c in currencies]
 
 
 def format_currency(value):
@@ -80,5 +134,5 @@ def write_file(file_path, content):
         file.write(content)
 
 
-__all__ = ["load_dataframe", "format_currency",
+__all__ = ["load_dataframe", "getExchangeRate", "currencyUSDvals", "format_currency",
            "calculate_percentage", "read_file", "write_file"]
